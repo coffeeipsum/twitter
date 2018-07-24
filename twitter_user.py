c9b48cc3264b3744bd110ff59539797866c42fca
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-07-24 08:55:09 +0000

    twitter user

---
+++ twitter_user.py
@@ -0,0 +1,23 @@
+import json
+import tweepy
+from tweepy import OAuthHandler
+
+
+CONSUMER_KEY = '8l2AuJLFehoqbikiPhHhfJpj8'
+CONSUMER_SECRET = 'HCG2Z6IdzrWms2R5VCTNaAGRTT0i51MTkzPPvQmRTg3dSWfL0S'
+OAUTH_TOKEN = '761230413209669636-tw2I4YVKRpPy6zWwSnQPiXlYhwOgpaE'
+OAUTH_TOKEN_SECRET = '4k0iTtNQQrHqkaMd8T33Jxre2gcx1D2SnWK7laUfZQrnY'
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+user = api.get_user('@BarackObama')
+
+print(user.screen_name)
+print(user.followers_count)
+
+for friend in user.friends():
+    print(friend.screen_name)
+    print(friend.followers_count)
