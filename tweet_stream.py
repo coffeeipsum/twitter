4532ef08610363e78bf0aa3ab1e57b46543f7c1d
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-07-24 09:46:09 +0000

    added limit to Tweet Stream

--- tweet_stream.py
+++ tweet_stream.py
@@ -13,6 +13,8 @@ OAUTH_TOKEN_SECRET = '4k0iTtNQQrHqkaMd8T33Jxre2gcx1D2SnWK7laUfZQrnY'
 
 keyword_list = ["python," "javascrip", "php", "C#"]  # to track list
 
+limit = 500
+
 class MyStreamListener(StreamListener):
     
     def __init__(self):
@@ -20,13 +22,17 @@ class MyStreamListener(StreamListener):
         self.num_tweets = 0
         
     def on_data(self, data):
-        try:
-            with open('tweet_mining.json', 'a') as tweet_file:
-                tweet_file.write(data)
-                return True
-        except BaseException as e:
-            print("Failed on_data: %s"%str(e))
-        return True
+        if self.num_tweets < limit:
+            self.num_tweets += 1
+            try:
+                with open('tweet_mining.json', 'a') as tweet_file:
+                    tweet_file.write(data)
+                    return True
+            except BaseException as e:
+                print("Failed on_data: %s"%str(e))
+            return True
+        else:
+            return False
         
         
     def on_error(self, status):
@@ -39,11 +45,3 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 twitter_stream = Stream(auth, MyStreamListener())
 twitter_stream.filter(track=keyword_list)
-
-
-""" NOTE -
-this The stream will run indefinitely(!) and fill the tweets JSON file with tweets until you stop the
-process.
-==> To stop the process we're going to use control-c. 
-
-"""
